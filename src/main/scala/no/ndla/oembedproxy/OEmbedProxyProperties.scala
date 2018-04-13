e5cc37c6a2eb0a59f3a6ad98cec1c5e96ecf5fa6
Jonas Natten <jonas.natten@knowit.no>
2018-04-13 10:26:38 +0200

    #972: Uses the prod h5p environment for brukertest

--- src/main/scala/no/ndla/oembedproxy/OEmbedProxyProperties.scala
+++ src/main/scala/no/ndla/oembedproxy/OEmbedProxyProperties.scala
@@ -38,9 +38,11 @@ object OEmbedProxyProperties {
   ).getOrElse(Environment, s"https?://ndla-frontend.$Environment.api.ndla.no/*")
 
   val NdlaH5POembedProvider = Map(
+    "brukertest" -> "https://h5p.ndla.no",
     "prod" -> "https://h5p.ndla.no"
   ).getOrElse(Environment, "https://h5p-test.ndla.no")
   val NdlaH5PApprovedUrl = Map(
+    "brukertest" -> "https://h5p.ndla.no/resource/*",
     "prod" -> "https://h5p.ndla.no/resource/*"
   ).getOrElse(Environment, "https://h5p-test.ndla.no/resource/*")
 
